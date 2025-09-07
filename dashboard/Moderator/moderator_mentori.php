777ff3e4d175f7218d221c5156162462f3d24c65
DESKTOP-4Q542\Administrator <marijamilinovic02@gmail.com>
2025-09-07 23:30:48 +0200

    Dodano prikazivanje osnovnih podataka u moderator_mentori.php

--- dashboard/Moderator/moderator_mentori.php
+++ dashboard/Moderator/moderator_mentori.php
@@ -27,5 +27,33 @@ $mentori = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 <h2>🧑‍🏫 Mentori koje ste unijeli</h2>
+
+<?php if (count($mentori) > 0): ?>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Ime</th>
+                <th>Prezime</th>
+                <th>Uloga u Kompaniji</th>
+                <th>Email</th>
+                <th>Telefon</th>
+                <th>Slika</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($mentori as $mentor): ?>
+                <tr>
+                    <td><?= htmlspecialchars($mentor['ime']); ?></td>
+                    <td><?= htmlspecialchars($mentor['prezime']); ?></td>
+                    <td><?= htmlspecialchars($mentor['uloga_kompanije']); ?></td>
+                    <td><?= htmlspecialchars($mentor['email']); ?></td>
+                    <td><?= htmlspecialchars($mentor['telefon']); ?></td>
+                    <td><?= htmlspecialchars($mentor['slika']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+<?php else: ?>
 <?php endif; ?>
 	<a href="dodaj_mentora.php" class="btn btn-primary mt-3">➕ Dodaj novog mentora</a>
